9354bad5b8f7b244874ccbfee741575982e9b13b
blanclemjk <bluemustard2805@gmail.com>
2018-12-02 17:01:39 -0500

    half tutorial + spark task python untested

---
+++ Freq.py
@@ -0,0 +1,41 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from pyspark.mllib.fpm import FPGrowth
+from pyspark import SparkContext
+import pyspark_cassandra
+
+if __name__ == "__main__":
+    import pyspark_cassandra
+
+    conf = SparkConf() \
+    	.setAppName("FP-growth spark-cassandra") \
+    	.setMaster("spark://spark-master:7077") \
+    	.set("spark.cassandra.connection.host", "cas-1")
+
+    sc = CassandraSparkContext(conf=conf)
+    
+    #data = sc.textFile("sample_fpgrowth.txt")
+    #transactions = data.map(lambda line: line.strip().split(' '))
+    transactions = sc.cassandraTable("receipts", "receipts") \
+                .select("products") \
+                .map(lambda r: list(r["products"].keys()))
+
+    model = FPGrowth.train(transactions, minSupport=0.2, numPartitions=10)
+    result = model.freqItemsets().collect()
+    for fi in result:
+        print(fi)
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Readme
 
 ## Setting up Cassandra
-
-Execute the following queries.
-
+1. Download and Install the latest Cassandra Distribution for Windows from [here](https://academy.datastax.com/planet-cassandra//cassandra)
+2. Run {installation dir}/bin/cassandra.bat
+3. Run {installation dir}/bin/cqlsh.bat  and execute the following queries:
 ```cql
 CREATE KEYSPACE receipts
 WITH replication = {
@@ -17,3 +17,41 @@ CREATE TABLE receipts.receipts (
 	PRIMARY KEY (id)
 );
 ```
+
+## Setting up Spark
+1. Download and install the latest distribution of spark-standalone/pyspark  from [here]()
+2. Run {installation dir}/bin/spark-class.cmd org.apache.spark.deploy.master.Master
+3. In 2 other separate command prompts, run {installation dir}/bin/spark-class.cmd org.apache.spark.deploy.worker.Worker spark://localhost:7077
+
+## Setting up the REST Server
+1. Navigate to the "REST Api" folder.
+2. Rename the `.env.example` file to `.env` and set the environment variables with your values.
+3. In a command prompt, execute the following commands:
+```
+npm i
+npm run serve
+```
+
+## Using the system through the REST API
+
+### Add receipt
+
+```cURL
+curl -X POST \
+  'http://localhost:3000' \
+  -H 'Content-Type: application/json' \
+  -d '{ "id": "c534cf02-e8f3-11e8-9f32-f2801f1b92d1", 
+        "products": { "Tomato": 10, "Banana": 123 }  
+      }'
+```
+
+### Get products
+
+```cURL
+curl -X GET 'http://localhost:3000'
+```
+
+### Get frequent products
+
+```cURL
+curl -X GET 'http://localhost:3000/frequent'
