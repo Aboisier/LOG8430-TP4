b67708eb72be9a2f285ec6a2514ade2dc74d83b7
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2018-12-03 04:11:48 -0500

    Added REST client CLI

--- rest_client.py
+++ rest_client.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import random
 import urllib2
@@ -26,6 +27,29 @@ ALL_PRODUCTS = BEER + CEREALS + FRUITS + PASTA + SPICES + VEGETABLES
 PRODUCTS_DICT = { 'beer': BEER, 'cereals': CEREALS, 'fruits': FRUITS, 
                   'pasta': PASTA, 'spices': SPICES, 'vegetables': VEGETABLES }
 
+def add_receipt(receipt):
+    url = 'http://localhost:3000'
+    headers = {'Content-Type': 'application/json'}
+    body = json.dumps(receipt)
+    req = urllib2.Request(url, body, headers)
+    res = urllib2.urlopen(req)
+
+def clear_database():
+    url = 'http://localhost:3000/clear'
+    req = urllib2.Request(url, '')
+    res = urllib2.urlopen(req)
+
+def get_all_products():
+    url = 'http://localhost:3000'
+    req = urllib2.Request(url)
+    res = urllib2.urlopen(req)
+    return res.read()
+        
+def get_frequent_products():
+    url = 'http://localhost:3000/frequent'
+    req = urllib2.Request(url)
+    res = urllib2.urlopen(req)
+    return res.read()
 
 def gen_receipt(nb_products, freq_type=None):
     products = []
@@ -38,35 +62,37 @@ def gen_receipt(nb_products, freq_type=None):
     products_dict = { product: random.randint(1, 100) for product in products }
     return { 'id': str(uuid.uuid4()), 'products': products_dict }
 
-def add_receipt(receipt):
-    url = 'http://localhost:3000'
-    headers = {'Content-Type': 'application/json'}
-    body = json.dumps(receipt)
-    req = urllib2.Request(url, body, headers)
-    res = urllib2.urlopen(req)
-
 def populate_database(nb_receipts, max_nb_products, freq_type=None):
+    clear_database()
+
+    if freq_type is None:
+        freq_type = random.choice(PRODUCTS_DICT.keys())
+
     for _ in range(nb_receipts):
         nb_products = random.randint(2, max_nb_products)
         receipt = gen_receipt(nb_products, freq_type)
         add_receipt(receipt)
 
-def clear_database():
-    url = 'http://localhost:3000/clear'
-    req = urllib2.Request(url, '')
-    res = urllib2.urlopen(req)
 
-def get_all_products():
-    url = 'http://localhost:3000'
-    req = urllib2.Request(url)
-    res = urllib2.urlopen(req)
-    print res.read()
-        
-def get_frequent_products():
-    url = 'http://localhost:3000/frequent'
-    req = urllib2.Request(url)
-    res = urllib2.urlopen(req)
-    print res.read()
+parser = argparse.ArgumentParser()
+parser.add_argument('action', choices=[ 'clear', 'frequent', 'get_all', 'populate'],
+                    help='The action to perform.')
+parser.add_argument('--type', choices=PRODUCTS_DICT.keys(),
+                    help='The procuct type that should be more frequent.')
+args = parser.parse_args()
 
-
+if args.action == 'clear':
+    print 'Clearing database...'
+    clear_database()
+    print 'Database cleared!'
+elif args.action == 'frequent':
+    print 'Getting frequent products...'
+    print get_frequent_products()
+elif args.action == 'get_all':
+    print 'Getting all products...'
+    print get_all_products()
+elif args.action == 'populate':
+    print 'Populating database...'
+    populate_database(100, 10, args.type)
+    print 'Database populated!'
 
